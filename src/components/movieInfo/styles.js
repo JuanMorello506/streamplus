5dc2c537e5af780fad3e77bd1c50b4759c685d49
samefede <federico.farinia@gmail.com>
2024-05-24 17:45:56 -0300

    styles for modal

--- src/components/movieInfo/styles.js
+++ src/components/movieInfo/styles.js
@@ -14,6 +14,12 @@ export const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
 	},
 	modalView: {
 		width: 350,
