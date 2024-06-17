94657760015d51c7e37b4574d1a2321452fc6acb
JuanMorello506 <morellojuan992@gmail.com>
2024-06-17 02:15:29 -0300

    add favouriteService  and favorite add and remove functionality. Mod PerfilData

---
+++ src/services/Favourites/favouritesService.js
@@ -0,0 +1,43 @@
+import { BASE_URL } from "../../config/config";
+
+export const getFavourites = async (id) => {
+    const response = await fetch(`${BASE_URL}/streamplus/favourites/${id}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("error", JSON.stringify(error));
+        return error;
+      });
+    return response;
+};
+
+export const addFavourites = async (userId, movieId) => {
+    const response = await fetch(`${BASE_URL}/streamplus/favourites/${userId}/${movieId}`, {
+      method: "POST",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("error", JSON.stringify(error));
+        return error;
+      });
+    return response;
+};
+
+export const removeFavourites = async (userId, movieId) => {
+    const response = await fetch(`${BASE_URL}/streamplus/favourites/${userId}/${movieId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("error", JSON.stringify(error));
+        return error;
+      });
+    return response;
+};
