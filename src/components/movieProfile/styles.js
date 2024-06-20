e014b3d7a05008846287095374397f9d438e9ea4
JuanMorello506 <morellojuan992@gmail.com>
2024-06-20 15:41:38 -0300

    add. CommentsService e implementacion de comentarios en MovieProfile

--- src/components/movieProfile/styles.js
+++ src/components/movieProfile/styles.js
@@ -16,6 +16,8 @@ export const styles = StyleSheet.create({
     margin: 10,
     fontSize: 17,
   },
+
+
   details: {
     backgroundColor: '#332B47',
     flex: 1,
@@ -76,10 +78,58 @@ export const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderWidth: 8,
   },
+ 
+
   commentsContainer: {
+    flex: 1,
+    marginTop: 20,
     borderTopColor: '#FFFFFF',
     borderStyle: 'solid',
+    borderColor:"white",
+},
+commentInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+},
+userAvatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+    marginLeft: 8,
+},
+commentInput: {
+    flex: 1,
     borderWidth: 1,
-    margin: 5,
-  },
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    
+},
+commentButton: {
+    marginLeft: 8,
+    marginRight: 8,
+    backgroundColor: '#8F78C6',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+},
+commentButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+},
+commentList: {
+    flex: 1,
+    marginLeft:15,
+},
+commentItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+},
+commentText: {
+    marginLeft: 10,
+    color: "white"
+},
 });
