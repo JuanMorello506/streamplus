52d4f369aa8972f6b2b290ae763ba825553d07d8
samefede <federico.farinia@gmail.com>
2024-06-23 14:10:06 -0300

    cambios de estilos

--- src/services/User/userService.js
+++ src/services/User/userService.js
@@ -13,3 +13,21 @@ export const getUserById = async (userId) => {
       });
     return response;
 };
+
+export const updateUser = async (userId, userName, email) => {
+    const response = await fetch(`${BASE_URL}/streamplus/user/${userId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userName, email }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("error", JSON.stringify(error));
+        return error;
+      });
+    return response;
+}
